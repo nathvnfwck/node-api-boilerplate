32dcf2dcb78c2e98d80228cee09630a6a294e300
Nathan Lima <nathaanscout@icloud.com>
2023-11-11 22:43:22 -0300

    feat: controller adapter ports & helpers

---
+++ src/adapters/presentation/controllers/helpers/http-response.ts
@@ -0,0 +1,27 @@
+import { type HttpResponse } from '../ports/http'
+import { ServerError } from '../errors/server-error'
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: { error: error.message }
+})
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
+
+export const created = (): HttpResponse => ({
+  statusCode: 201,
+  body: undefined
+})
+
+export const notFound = (): HttpResponse => ({
+  statusCode: 204,
+  body: null
+})
+
+export const serverError = (reason: string): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(reason)
+})
