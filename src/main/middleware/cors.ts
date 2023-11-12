7d92c40be3691268a6aa67ba8061acbf381424e9
Nathan Lima <nathaanscout@icloud.com>
2023-11-11 22:43:48 -0300

    feat: cors middleware

---
+++ src/main/middleware/cors.ts
@@ -0,0 +1,8 @@
+import { type Request, type Response, type NextFunction } from 'express'
+
+export const cors = (request: Request, response: Response, next: NextFunction): void => {
+  response.set('access-control-allow-origin', '*')
+  response.set('access-control-allow-headers', '*')
+  response.set('access-control-allow-methods', '*')
+  next()
+}
