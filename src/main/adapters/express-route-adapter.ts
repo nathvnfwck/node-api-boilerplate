51db691d389930f9b85d5f77c39f80e01a614d6a
Nathan Lima <nathaanscout@icloud.com>
2023-11-11 22:43:56 -0300

    feat: express route adapter

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,21 @@
+import { type Request, type Response } from 'express'
+
+import { type Controller } from '@/adapters/presentation/controllers/ports/controller'
+import { type HttpRequest } from '@/adapters/presentation/controllers/ports/http'
+
+export const adaptRoute = (controller: Controller) => {
+  return async (request: Request, response: Response) => {
+    const httpRequest: HttpRequest = {
+      body: {
+        ...request.body,
+        ...request.query
+      }
+    }
+
+    const httpResponse = await controller.handle(httpRequest)
+
+    response.status(httpResponse.statusCode)
+
+    if (httpRequest.body) response.json(httpResponse.body)
+  }
+}
